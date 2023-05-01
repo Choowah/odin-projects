90246907bb299e66e19e7e2cdbaaa3e76fd64559
Choowah <yeeyong0911@gmail.com>
2023-05-01 22:04:05 +0800

    Add click selection and scorecount functionality

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,102 +1,108 @@
-// //Generate computer signal using random
-// // 0 signifying rock
-// // 1 signifying paper
-// // 2 signifying scissors
+//Generate computer signal using random
+// 0 signifying rock
+// 1 signifying paper
+// 2 signifying scissors
 
-// let computerSignal
-// let formattedPlayerChoice
-// let result
-// let playerScore = 0
-// let computerScore = 0
+let computerSignal
+let playerSignal
+let result
+let playerScore = 0
+let computerScore = 0
 
-// function getComputerChoice(){
-//     let computerChoice = Math.floor(Math.random()*3)
-//     if (computerChoice === 0){
-//         computerSignal = "rock"
-//     } else if (computerChoice === 1){
-//         computerSignal = "paper"
-//     } else {
-//         computerSignal = "scissors"
-//     }
-//     return computerSignal;
-// }
+function getComputerChoice(){
+    let computerChoice = Math.floor(Math.random()*3)
+    if (computerChoice === 0){
+        computerSignal = "rock"
+    } else if (computerChoice === 1){
+        computerSignal = "paper"
+    } else {
+        computerSignal = "scissors"
+    }
+    return computerSignal;
+}
 
 // // Obtain player choice
 
-// function getPlayerChoice(){
-//     let playerChoice = prompt("Pick Rock / Paper / Scissors")
-//     formattedPlayerChoice = playerChoice.toLowerCase();
-//     if (formattedPlayerChoice === "rock" || formattedPlayerChoice === "paper" || formattedPlayerChoice === "scissors"){
-//         return formattedPlayerChoice;
-//     } else {
-//         alert("Please only enter: Rock / Paper/ Scissors")
-//         //Will need the code to loop back to another choice without having to invoke the function again in the console
-//     }
-// }
+let rock = document.getElementById("rock")
+rock.addEventListener("click", playerChoice);
 
-// // Create function taking two argument and generate winner or draw
+let paper = document.getElementById("paper")
+paper.addEventListener("click", playerChoice)
 
-// function compareChoices(formattedPlayerChoice,computerSignal){
-//     if (formattedPlayerChoice === "rock"){
-//         if (computerSignal === "rock") {
-//             result = "It's a tie!"
-//             return result
-//         } else if (computerSignal === "paper"){
-//             result = "You lost!"
-//             computerScore++;
-//             return result
-//         } else if (computerSignal === "scissors"){
-//             result = "You won!"
-//             playerScore++;
-//             return result
-//         } else {
-//             result = "Issues with the computerSignal"
-//         }
-//     } else if (formattedPlayerChoice === "paper") {
-//         if (computerSignal === "rock") {
-//             result = "You won!"
-//             playerScore++;
-//             return result
-//         } else if (computerSignal === "paper"){
-//             result = "It's a tie!"
-//             return result
-//         } else if (computerSignal === "scissors"){
-//             result = "You lost!"
-//             computerScore++;
-//             return result
-//         } else {
-//             result = "Issues with the computerSignal"
-//         }
-//     } else if (formattedPlayerChoice === "scissors") {
-//         if (computerSignal === "rock") {
-//             result = "You lost!"
-//             computerScore++;
-//             return result
-//         } else if (computerSignal === "paper"){
-//             result = "You won!"
-//             playerScore++;
-//             return result
-//         } else if (computerSignal === "scissors"){
-//             result = "It's a tie!"
-//             return result
-//         } else {
-//             result = "Issues with the computerSignal"
-//         }
-//     } else {
-//         alert("formattedPlayerChoice is having issues")
-//     }
-// }
+let scissors = document.getElementById("scissors")
+scissors.addEventListener("click", playerChoice)
 
-// for (let i = 1; (playerScore < 5 && computerScore < 5); i++) {
-//     getComputerChoice();
-//     getPlayerChoice();
-//     compareChoices(formattedPlayerChoice,computerSignal);
-//     // Console.log result (player / computer win / draw)
+let announcement = document.querySelector("p")
 
-//     let resultAlert = ("You chose: " + formattedPlayerChoice + "\nComputer chose: " + computerSignal + "\n" + result)
-//     console.log(resultAlert);
-//     console.log("Player Score: " + playerScore + " | Computer Score: " + computerScore);
-//   }
+function playerChoice(e){
+    if(e.target.parentElement.id === "rock" || e.target.parentElement.id === "paper" || e.target.parentElement.id === "scissors"){
+    console.log(e.target.parentElement.id)
+    playerSignal = e.target.parentElement.id;
+
+    getComputerChoice();
+    compareChoices(playerSignal,computerSignal);
+
+    let playerScoreboard = document.querySelector(".score")
+    playerScoreboard.textContent = playerScore
+    
+    let computerScoreboard = document.querySelectorAll(".score")[2]
+    computerScoreboard.textContent = computerScore
+    }
+}
+
+// Create function taking two argument and generate winner or draw
+
+function compareChoices(playerSignal,computerSignal){
+    if (playerSignal === "rock"){
+        if (computerSignal === "rock") {
+            announcement.textContent = "It's a Tie!"
+        } else if (computerSignal === "paper"){
+            announcement.textContent = "You lost!"
+            computerScore++;
+            return result
+        } else if (computerSignal === "scissors"){
+            announcement.textContent = "You won!"
+            playerScore++;
+            return result
+        } else {
+            result = "Issues with the computerSignal"
+        }
+    } else if (playerSignal === "paper") {
+        if (computerSignal === "rock") {
+            announcement.textContent = "You won!"
+            playerScore++;
+            return result
+        } else if (computerSignal === "paper"){
+            announcement.textContent = "It's a tie!"
+            return result
+        } else if (computerSignal === "scissors"){
+            announcement.textContent = "You lost!"
+            computerScore++;
+            return result
+        } else {
+            result = "Issues with the computerSignal"
+        }
+    } else if (playerSignal === "scissors") {
+        if (computerSignal === "rock") {
+            announcement.textContent = "You lost!"
+            computerScore++;
+            return result
+        } else if (computerSignal === "paper"){
+            announcement.textContent = "You won!"
+            playerScore++;
+            return result
+        } else if (computerSignal === "scissors"){
+            announcement.textContent = "It's a tie!"
+            return result
+        } else {
+            result = "Issues with the computerSignal"
+        }
+    } else {
+        alert("codes is having issues")
+    }
+}
+
+// Alerting Winning Conditions
 
 // if (playerScore === 5){
 //     alert("Player won!")
@@ -105,3 +111,4 @@
 // } else {
 //     alert("Please debug")
 // }
+
