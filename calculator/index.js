f8ec990c6421dfc0c6eaba7e0f4202f2f4c0db1d
Choowah <yeeyong0911@gmail.com>
2023-05-03 17:14:41 +0800

    Calculator Initial version

---
+++ calculator/index.js
@@ -0,0 +1,87 @@
+function add(a,b){
+    return (parseFloat(a)+parseFloat(b)).toFixed(3);
+}
+
+function subtract(a,b){
+    return parseFloat(a)-parseFloat(b);
+}
+
+function multiply(a,b){
+    return parseFloat(a)*parseFloat(b);
+}
+
+function divide(a,b){
+    return parseFloat(a)/parseFloat(b);
+}
+
+let firstNumber; // firstNumber
+let secondNumber; // secondNumber
+let operator; //calculate operation (add / subtract / multiply / divide)
+let result;
+
+function operate(a,b,x){
+    secondNumber = displayValue;
+
+    if (operator === "add"){
+        result = add(firstNumber,secondNumber)
+    } else if (operator === "subtract") {
+        result = subtract(firstNumber,secondNumber)
+    } else if (operator === "multiply") {
+        result = multiply(firstNumber,secondNumber)
+    } else if (operator === "divide") {
+        result = divide(firstNumber,secondNumber)
+    } else {
+        alert ("Operator is having problems")
+    }
+
+    calc.textContent += " " + secondNumber;
+    input.textContent = result;
+
+    // console.log(firstNumber + " " + secondNumber + " " + operator)
+    // console.log(`${operator}`(firstNumber,secondNumber));
+}
+
+let displayValue = "";
+
+const one = document.querySelectorAll(".button")[10]
+const two = document.querySelectorAll(".button")[11]
+const three = document.querySelectorAll(".button")[12]
+const four = document.querySelectorAll(".button")[6]
+const five = document.querySelectorAll(".button")[7]
+const six = document.querySelectorAll(".button")[8]
+const seven = document.querySelectorAll(".button")[2]
+const eight = document.querySelectorAll(".button")[3]
+const nine = document.querySelectorAll(".button")[4]
+const zero = document.querySelectorAll(".button")[15]
+const decimal = document.querySelectorAll(".button")[14]
+const addButton = document.querySelectorAll(".button")[17]
+const subtractButton = document.querySelectorAll(".button")[13]
+const multiplyButton = document.querySelectorAll(".button")[9]
+const divideButton = document.querySelectorAll(".button")[5]
+const equal = document.querySelectorAll(".button")[16]
+const input = document.querySelector(".input")
+const calc = document.querySelector(".calculating")
+const numbers = [one, two, three, four, five, six, seven, eight, nine, zero, decimal]
+const operators = [addButton, subtractButton, multiplyButton, divideButton]
+
+numbers.forEach(number => number.addEventListener("click", display))
+operators.forEach(operator => operator.addEventListener("click", operatorClick))
+equal.addEventListener("click",operate)
+
+function display(e){
+    if(displayValue.includes(".") && e.srcElement.textContent === "."){
+        console.log(e.srcElement.textContent)
+        displayValue = displayValue.slice(0,-1);
+    }
+    displayValue += e.srcElement.textContent;
+    input.textContent = displayValue;
+}
+
+function operatorClick(e){
+    firstNumber = displayValue;
+    calc.textContent = displayValue + " " + e.srcElement.textContent;
+    displayValue = "";
+    input.textContent = 0;
+    operator = e.target.id;
+    // Collect secondNumber
+}
